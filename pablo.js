809ca31faf0961cd6f7f78c02f9199f429a91158
Pablo351 <paex94@gmail.com>
2021-03-02 20:53:52 -0300

    Ejercios resueltos en pablo.js

---
+++ pablo.js
@@ -0,0 +1,151 @@
+ /*************************************
+*
+*             Ejercicio 1
+*
+**************************************/
+
+// console.log('Pablo')
+// console.log('Exequiel')
+// console.log('Medina')
+
+ /*************************************
+*
+*             Ejercicio 2
+*
+**************************************/
+
+// let name = 'Pablo'
+// let lastName= 'Medina'
+// let age = 26
+// let nacionality = 'Argentina'
+// let myPetName = 'Manuelita'
+
+// console.log(`Mi nombre es`, name, lastName)
+// console.log(`tengo`, age, `años`)
+// console.log(`Soy de`, nacionality)
+// console.log(`Y mi tortuga se llama`, myPetName)
+// console.log(`Mi nombre es`, name, lastName, `y vivo en`, nacionality)
+
+ /*************************************
+*
+*             Ejercicio 3
+*
+**************************************/
+
+// let name = 'Pablo'
+// let age = 26
+// let nacionality = 'Argentina'
+// console.log(`Hola me llamo ${name}, el doble de mi edad es ${age * 2} y soy de ${nacionality}`)
+
+// let esMayor = age > 90
+// console.log(esMayor)
+
+// let signo = null
+// let astro = undefined
+// console.log(signo, astro)
+// console.log(`Hola me llamo ${signo} y tengo ${astro} años`)
+
+ /*******************************************************************
+*
+*             Ejercicio 6 (4 y 5 no tienen ejercitacion)
+*
+*********************************************************************/
+
+// console.log( 5 > 4) //true
+// console.log('apple' > 'pineapple') //false
+// console.log('2'>'12') //true
+// console.log(undefined==null) //true
+// console.log(undefined===null) //false
+
+
+ /*************************************
+*
+*             Ejercicio 7
+*
+**************************************/
+
+// let password = 'myOldPassword'
+// if (password == 'MyPassword1234') {
+//     console.log('Logged in')
+// } else {
+//     console.log('Wrong Passwod')
+// }
+
+// password =='MyPassword1234' ? console.log('Logged in') : console.log('WrongPassword')
+
+ /*************************************
+*
+*             Ejercicio 8
+*
+**************************************/
+
+// console.log( null || 2 || undefined ) // 2
+// console.log( 1 && null && 2 ) //null
+// console.log( !'Hola' ) //false
+// console.log( null || 2 && 3 || 4 ) //3
+
+ /*************************************
+*
+*             Ejercicio 9
+*
+**************************************/
+
+// for (let i = 0; i <= 10; i+=2) {
+//     console.log(i)
+// }
+
+// let i = 0;
+// while (i < 3) {
+//   console.log( `numero ${i}!` );
+//   i++;
+// }
+
+// for (let i= 0; i<3; i++) {
+//     console.log(`numero ${i}!`)
+    
+// }
+
+ /*************************************
+*
+*             Ejercicio 10
+*
+*************************************/
+
+
+////1
+// let navegador = 'asd'
+
+// if (navegador == 'Edge') {
+//     console.log('Tienes Edge!');    
+//     } else { 
+//             if (navegador == 'Chrome' || navegador == 'Firefox' || navegador == 'Safari' || navegador == 'Opera') {
+//                 console.log('Esta bien, soportamos estos navegadores tambien');
+//             } else {
+//                 console.log('Esperamos que esta pagina se vea bien');
+//             }
+//     }
+
+
+// //2
+// let a = 4
+
+// switch (a) {
+//     case 0:
+//         console.log(0);
+//         break;
+
+//     case 1:
+//         console.log(1);
+//         break;
+    
+//     case 2:
+//         console.log(2);
+//         break;
+    
+//     case 3:
+//         console.log(3);
+//         break;
+    
+//     default:
+//         console.log('Ninguno de los anteriores');
+// }
